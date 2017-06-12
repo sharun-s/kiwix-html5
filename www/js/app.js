7bc6af7f3258a9c96860130c2a8c760e3b6cfa3f
sharun-s <sharun.msgs@gmail.com>
2017-06-12 10:40:15 +0530

    Refactoring app.js to make it simple to load archive without gui to simplify testing Some initial code to load archive from session storage Grouped handlers and init code into setupHandlers and initKiwix
    Added readXHRSlice to util.js

--- www/js/app.js
+++ www/js/app.js
@@ -1016,4 +1016,28 @@ define(['jquery', 'zimArchiveLoader', 'util', 'uiUtil', 'cookies','abstractFiles
         });
     }
 
+    function storageAvailable(type) {
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch(e) {
+            return false;
+        }
+    }
+
+    /*if (storageAvailable('sessionStorage')) {
+        if(!sessionStorage.getItem('archive')) {
+            //populateStorage();
+        } else {
+          //setStyles();
+        }
+    }
+    else {*/
+        initKiwix();
+    /*}*/
+
 });
