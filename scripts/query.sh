1b71793a082adf1992c03fe9e60f503600142be4
sharun-s <sharun.msgs@gmail.com>
2018-05-08 11:44:08 +0530

    Added an archive script parameter. Can now query index of different archives based on this arg (pass wiki/quotes etc if index is in wiki.db/quotes.db etc).

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-usage() { echo -e "Usage: $0 Keyword [-i|u] [-x]\n \
+usage() { echo -e "Usage: $0 [-i|u] [-x] ARCHIVE KEYWORD\n \
 \t If just Keyword provided, query runs over the title index [i.e. title search]\n \
 \t-i [image url search] query over url index ignoring articles and redirects\n \
 \t-u [url search] query over url index\n
@@ -7,7 +7,7 @@ usage() { echo -e "Usage: $0 Keyword [-i|u] [-x]\n \
 
 #KEYWORD=$1
 #ARCHIVE=$2
-QUERY="select cluster, blob, namespace, url from quotes "
+QUERY="select cluster, blob, namespace, url from "
 IDX=0
 BROWSERPATH='C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe'
 KIWIXPATH='file:///C:/<some path>/www/index.html'
@@ -26,8 +26,9 @@ done
 
 #echo "key:${KEYWORD} optind:${OPTIND}"
 shift $((OPTIND-1))
-KEYWORD=$1
-#echo "key:${KEYWORD} optind:${OPTIND}"
+ARCHIVE=$1
+KEYWORD=$2
+#echo "key:${ARCHIVE} ${KEYWORD} optind:${OPTIND}"
 CLAUSE=(
 	"where redirect='false' and title like '$KEYWORD' limit 1"
 	"where redirect='false' and url like '%$KEYWORD%' limit 1"
@@ -38,12 +39,12 @@ CLAUSE=(
 if [ -z "${KEYWORD}" ]; then
 	usage;
 else
-	QUERY="${QUERY}${CLAUSE[$IDX]}";
+	QUERY="${QUERY}${ARCHIVE} ${CLAUSE[$IDX]}";
 fi
 echo "${QUERY}"
 if [ $x ]; then  
-./sqlite3.exe quotes.db "${QUERY}" | awk -F '|' '{ printf "archive=quotes&c=%s&b=%s&n=%s&url=%s",$1,$2,$3,$4 }' ;
+./sqlite3.exe ${ARCHIVE}.db "${QUERY}" | awk -v ar="$ARCHIVE" -F '|' '{ printf "archive=%s&c=%s&b=%s&n=%s&url=%s",ar,$1,$2,$3,$4 }' ;
 else
-./sqlite3.exe quotes.db "${QUERY}" | awk -v bp="$BROWSERPATH" -v kp="$KIWIXPATH" -F '|' '{ printf "\"%s\" -url \"%s?archive=quotes&c=%s&b=%s&n=%s&url=%s\"",bp,kp,$1,$2,$3,$4 }' | sh ;
+./sqlite3.exe "${ARCHIVE}".db "${QUERY}" | awk -v ar="$ARCHIVE" -v bp="$BROWSERPATH" -v kp="$KIWIXPATH" -F '|' '{ printf "\"%s\" -url \"%s?archive=%s&c=%s&b=%s&n=%s&url=%s\"",bp,kp,ar,$1,$2,$3,$4 }' | sh ;
 fi
 exit 0
